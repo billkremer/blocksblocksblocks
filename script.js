35710df144723e4b01f86cc33356903721fa4777
Bill Kremer <bkrems12@gmail.com>
2017-01-10 17:23:57 -0600

    working product

---
+++ script.js
@@ -0,0 +1,24 @@
+ $(function () {
+   $('button').click( function () {
+
+     $('#putBlocksHere').append('<div class="newBox" style="background-color:' + randColorString() +'""><p>x</p></div>');
+   });  // appends a newBox to the DOM
+
+   $('#putBlocksHere').on('click','.newBox' ,function () {
+     $(this).css('background-color', 'black');
+   }); // changes the blocks black when clicked
+
+   $('#putBlocksHere').on('click', 'p', function () {
+     $(this).parents('div').remove();
+   });
+
+ });
+
+ function randColorString () {
+ // creates a random rgb string for putting inline to the newBox
+ var rand256a =  Math.floor(Math.random() * 256);
+ var rand256b =  Math.floor(Math.random() * 256);
+ var rand256c =  Math.floor(Math.random() * 256);
+
+ return "rgb(" + rand256a + "," + rand256b + "," + rand256c + ")"
+ };
